f10468e184fb7a746ba57818b4e329eb6fd92f25
abdulazim840 <aaz26@mail.aub.edu>
2022-05-06 15:34:46 +0300

    'ui'

---
+++ Assets/Scripts/main_menu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu : MonoBehaviour
+{
+    public void LevelOne(){
+        SceneManager.LoadScene(1);
+    }
+}
